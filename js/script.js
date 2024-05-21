b7a9c814dbb57b67d73a89570779218d35724e0a
Iwan241047 <241047@virtualwindow.co.za>
2024-05-21 14:13:04 +0200

    Class 2 T1

---
+++ js/script.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+console.log("I am " + 77 + " years old");
+console.log(5 + 7);
+
+const pi = 3.14;
+let person = "John Dov";
+let dog = "Hunter", age = 15, subject = "development";
+
+function myFunction(){
+    alert("How are you?");
+}
